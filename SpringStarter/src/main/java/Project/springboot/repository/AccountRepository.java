89f6cda45bd1ba8fdc8d946f6fd37a8af225cd36
VANSHPAR <159923891+VANSHPAR@users.noreply.github.com>
2025-03-20 20:00:16 +0530

    Add files via upload

---
+++ SpringStarter/src/main/java/Project/springboot/repository/AccountRepository.java
@@ -0,0 +1,13 @@
+package Project.springboot.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import Project.springboot.models.Account;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, Long> {
+    
+
+    
+}
