c10c7200908a5be083d3614a26985464810668c3
VANSHPAR <159923891+VANSHPAR@users.noreply.github.com>
2025-03-25 18:58:59 +0530

    Add files via upload

--- SpringStarter/src/main/java/Project/springboot/services/AccountService.java
+++ SpringStarter/src/main/java/Project/springboot/services/AccountService.java
@@ -1,6 +1,7 @@
 package Project.springboot.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import Project.springboot.models.Account;
@@ -11,7 +12,11 @@ public class AccountService {
     @Autowired
     private AccountRepository accountRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public Account save(Account account){
+        account.setPassword(passwordEncoder.encode(account.getPassword()));
         return accountRepository.save(account);
         
     }
