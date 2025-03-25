9698b00ee5723759c2eba160074b27babf1f365d
VANSHPAR <159923891+VANSHPAR@users.noreply.github.com>
2025-03-25 20:46:49 +0530

    Add files via upload

--- SpringStarter/src/main/java/Project/springboot/repository/AccountRepository.java
+++ SpringStarter/src/main/java/Project/springboot/repository/AccountRepository.java
@@ -1,5 +1,7 @@
 package Project.springboot.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +10,6 @@ import Project.springboot.models.Account;
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
     
-
+     Optional<Account> findOneByEmailIgnoreCase(String email);
     
 }
