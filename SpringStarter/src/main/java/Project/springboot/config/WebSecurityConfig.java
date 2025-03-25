9bdb27f149663ccc3036766198d012a792d074a3
VANSHPAR <159923891+VANSHPAR@users.noreply.github.com>
2025-03-25 20:18:57 +0530

    Add files via upload

--- SpringStarter/src/main/java/Project/springboot/config/WebSecurityConfig.java
+++ SpringStarter/src/main/java/Project/springboot/config/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package Project.springboot.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -48,9 +49,8 @@ public class WebSecurityConfig {
         http.logout(logout -> logout.logoutUrl("/logout")
         .logoutSuccessUrl("/logout?success"));
 
-        
-       
-       
+        http.httpBasic(Customizer.withDefaults());
+    
 
         
 
