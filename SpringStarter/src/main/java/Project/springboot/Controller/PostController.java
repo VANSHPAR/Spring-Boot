4006dbc9540c20df93817ae64c631af6f7843c16
VANSHPAR <159923891+VANSHPAR@users.noreply.github.com>
2025-05-02 20:40:45 +0530

    Add files via upload

--- SpringStarter/src/main/java/Project/springboot/Controller/PostController.java
+++ SpringStarter/src/main/java/Project/springboot/Controller/PostController.java
@@ -1,5 +1,6 @@
 package Project.springboot.Controller;
 
+import java.security.Principal;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,13 +19,23 @@ public class PostController {
     private PostService postService;
 
     
-    @GetMapping("/posts/{id}")
-    public String getPost(@PathVariable Long id,Model model){
+    @GetMapping("/post/{id}")
+    public String getPost(@PathVariable Long id,Model model,Principal principal){
         Optional<Post> optionalpost = postService.getById(id);
+        String authuser="email";
 
         if(optionalpost.isPresent()){
             Post post=optionalpost.get();
            model.addAttribute("post", post);
+           if(principal != null){
+            authuser=principal.getName(); //principal.getName return username
+           }
+           if(authuser.equals(post.getAccount().getEmail())){
+            model.addAttribute("isOwner", true);
+           }
+           else{
+            model.addAttribute("isOwner",false);
+           }
             return "post_views/post";
         }
         else {
@@ -32,8 +43,11 @@ public class PostController {
         }
    
     }
-
-
+    
+    @GetMapping("/posts/add")
+    public String addPost(Model model){
+        return "post_views/post_add";
+    }
 
 
 }
